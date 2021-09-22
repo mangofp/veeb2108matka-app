d0d9fffa6f5214778858f1214ca3155364aaabfe
Andres Järviste <andres.jarviste@wbscodingschool.com>
2021-09-22 11:01:01 +0300

    loeng12

--- index.js
+++ index.js
@@ -2,8 +2,69 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 8000
 
+const matk1 = {
+  id: 0,
+  nimetus: "Kevadmatk Kõrvemaal",
+  kirjeldus: "Lähme ja kõnnime kolm päeva looduses",
+  pildiUrl: '/pildid/matkaja.png',
+  osalejad: []
+}
+const matk2 = {
+  id: 1,
+  nimetus: "Rattamatk Jõgevamaal",
+  kirjeldus: "Väntame iga päev 30 kilomeetrit",
+  pildiUrl: '/pildid/rattamatk.jpg',
+  osalejad: []
+}
+const matk3 = {
+  id: 2,
+  nimetus: "Kepikõnnimatk ümber Tartu",
+  kirjeldus: "14 kilomeetrine jalutuskäik",
+  pildiUrl: '/pildid/matk_tartus1.jpg',
+  osalejad: []
+}
+const matk4 = {
+  id: 3,
+  nimetus: "Kepikõnnimatk ümber Pühajärve",
+  kirjeldus: "14 kilomeetrine jalutuskäik",
+  pildiUrl: '/pildid/matk_tartus1.jpg',
+  osalejad: []
+}
+
+const matkad = [matk1, matk2, matk3, matk4]
+
+const uudised = [
+  {
+    id:0,
+    pealkiri: "Uudis 1",
+    tekst: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas dolore fugiat earum cum libero exercitationem fugit facere voluptatibus incidunt, illo iste eos. Facilis veritatis quos molestias dicta itaque rerum!"
+  },
+  {
+    id:1,
+    pealkiri: "Uudis 2",
+    tekst: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas dolore fugiat earum cum libero exercitationem fugit facere voluptatibus incidunt, illo iste eos. Facilis veritatis quos molestias dicta itaque rerum!"
+  }
+]
+
+
 function esileht(req, res) {
-  return res.render("pages/index")
+  return res.render("pages/index", {matkad})
+}
+
+function naitaRegistreerumist(req, res) {
+  console.log("Matka number on: " + req.params.matk)
+  console.log("Matka andmed on")
+  const matk = req.params.matk
+  console.log(matkad[matk])
+  return res.render("pages/registreeru", {matk: matkad[matk]})
+} 
+
+function registreeriOsaleja(req, res) {
+  const matk = matkad[req.params.matk]
+  console.log(matk)
+  matk.osalejad.push(req.query)
+  console.log(matk)
+  return res.send("Registreeruti")
 }
 
 express()
@@ -14,5 +75,6 @@ express()
 .get('/', esileht)
 .get('/kontakt', (req, res) => res.render("pages/kontakt"))
 .get('/uudised', (req, res) => res.render("pages/uudised"))
-.get('/registreeru', (req, res) => res.render("pages/registreeru"))
+.get('/registreeru/:matk', naitaRegistreerumist)
+.get('/registreerimine/:matk', registreeriOsaleja)
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- public/matkaklubi.js
+++ public/matkaklubi.js
@@ -8,31 +8,8 @@ function looMatkaHtml(matk) {
             <p>
                 ${matk.kirjeldus}
             </p>
-            <a href="#" class="btn btn-primary">Registreeru</a>
+            <a href="/registreeru/${matk.id}" class="btn btn-primary">Registreeru</a>
         </div>
     </div>    
     `
-}
-
-const matk1 = {
-    nimetus: "Kevadmatk Kõrvemaal",
-    kirjeldus: "Lähme ja kõnnime kolm päeva looduses",
-    pildiUrl: './pildid/matkaja.png'
-}
-const matk2 = {
-    nimetus: "Rattamatk Jõgevamaal",
-    kirjeldus: "Väntame iga päev 30 kilomeetrit",
-    pildiUrl: './pildid/rattamatk.jpg'
-}
-const matk3 = {
-    nimetus: "Kepikõnnimatk ümber Tartu",
-    kirjeldus: "14 kilomeetrine jalutuskäik",
-    pildiUrl: './pildid/matk_tartus1.jpg'
-}
-
-const matkad = [matk1, matk2, matk3]
-document.getElementById('matkad').innerHTML = ''
-for (const matk of matkad) {
-    document.getElementById('matkad').innerHTML += looMatkaHtml(matk)
-    
 }
